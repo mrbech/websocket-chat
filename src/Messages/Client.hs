2e428b81d491ce239100793a0f2b0fe941fe5ac5
Martin Bech <martin@bech.io>
2021-02-06 21:55:52 +0100

    removed json helpers from Messages

--- src/Messages/Client.hs
+++ src/Messages/Client.hs
@@ -3,16 +3,11 @@
 module Messages.Client (
     RequestUsername(..),
     ChatMessage(..),
-    encodeRequestUsername,
-    decodeRequestUsername,
-    encodeChatMessage,
-    decodeChatMessage
 ) where
 
 import qualified Data.Aeson as J
 import GHC.Generics
 import Data.Text (Text)
-import Messages.Utils (decode'', encode'')
 
 -- | Client Message Data Types
 -- Following are the data types for messages that the client can send to the server.
@@ -22,20 +17,11 @@ import Messages.Utils (decode'', encode'')
 newtype RequestUsername = RequestUsername { username :: Text } 
     deriving (Generic, Show)
 
-encodeRequestUsername :: RequestUsername -> Text
-encodeRequestUsername = encode''
-decodeRequestUsername :: Text -> Maybe RequestUsername
-decodeRequestUsername = decode''
-
 -- Client sends a message to the chat.
 newtype ChatMessage = ChatMessage { message :: Text }
     deriving (Generic, Show)
 
-encodeChatMessage :: ChatMessage -> Text
-encodeChatMessage = encode''
-decodeChatMessage :: Text -> Maybe ChatMessage
-decodeChatMessage = decode''
-
+-- JSON boilerplate
 instance J.FromJSON RequestUsername
 instance J.ToJSON RequestUsername where
     toEncoding = J.genericToEncoding J.defaultOptions
--- src/Messages/Server.hs
+++ src/Messages/Server.hs
@@ -7,18 +7,11 @@ module Messages.Server (
     Message(..),
     UserJoined(..),
     UserLeft(..),
-    encodeUserConnected,
-    decodeUserConnected,
-    encodeRequestUsernameResponse,
-    decodeRequestUsernameResponse,
-    encodeChat,
-    decodeChat
 ) where
 
 import qualified Data.Aeson as J
 import GHC.Generics
 import Data.Text (Text)
-import Messages.Utils (decode'', encode'')
 
 -- | Server Message Data Types
 -- Following are the data types for messages that the server can send to a client.
@@ -26,18 +19,8 @@ import Messages.Utils (decode'', encode'')
 -- Messages that server sends when the user connects
 data UserConnected = RequestUsername deriving (Generic, Show)
 
-encodeUserConnected :: UserConnected -> Text
-encodeUserConnected = encode''
-decodeUserConnected :: Text -> Maybe UserConnected
-decodeUserConnected = decode''
-
 data RequestUsernameResponse = UsernameAccepted | UsernameAlreadyTaken
     deriving (Generic, Show)
-
-encodeRequestUsernameResponse :: RequestUsernameResponse -> Text
-encodeRequestUsernameResponse = encode''
-decodeRequestUsernameResponse :: Text -> Maybe RequestUsernameResponse
-decodeRequestUsernameResponse = decode''
 
 -- Possible messages sent to clients during chat
 data Chat = 
@@ -45,11 +28,6 @@ data Chat =
     | ChatUserJoined UserJoined
     | ChatUserLeft UserLeft
     deriving (Generic, Show)
-
-encodeChat :: Chat -> Text
-encodeChat = encode''
-decodeChat :: Text -> Maybe Chat
-decodeChat = decode''
 
 data Message = Message { fromUsername :: Text, message :: Text }
     deriving (Generic, Show)
@@ -62,15 +40,12 @@ newtype UserLeft = UserLeft { usernameLeft :: Text }
 instance J.FromJSON UserConnected
 instance J.ToJSON UserConnected where
     toEncoding = J.genericToEncoding J.defaultOptions
-
 instance J.FromJSON RequestUsernameResponse 
 instance J.ToJSON RequestUsernameResponse where
     toEncoding = J.genericToEncoding J.defaultOptions
-
 instance J.FromJSON Chat
 instance J.ToJSON Chat where
     toEncoding = J.genericToEncoding J.defaultOptions
-
 instance J.FromJSON Message
 instance J.ToJSON Message where
     toEncoding = J.genericToEncoding J.defaultOptions
